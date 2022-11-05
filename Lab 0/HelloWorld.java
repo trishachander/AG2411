18bda4783f880f06d1e88234fdda61f35e7e711e
Trisha Chander <50607144+trishachander@users.noreply.github.com>
2022-11-05 19:58:22 +0530

    Add files via upload

---
+++ Lab 0/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		boolean b;
+		int i;
+		String s;
+		b = true;
+		i = 0;
+		s = "Hello";
+
+		System.out.println(b);
+		System.out.println(i)
+		System.out.println(s);
+	}
+}
